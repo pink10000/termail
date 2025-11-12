bf0d1b68d749aa1b0f0e8255036e89503709ec25
Nick <nick-ls@users.noreply.github.com>
2025-11-11 17:40:40 -0800

    this is garbage

--- src/plugins/plugins.rs
+++ src/plugins/plugins.rs
@@ -1,5 +1,25 @@
-use wasmtime::component::{bindgen};
+use wasmtime::component::{Component, Linker, bindgen};
+use wasmtime::{Engine, Store};
 
 bindgen!("plugin");
 
-use tm::fuck;
+fn main() -> Result<(),()> {
+    let engine = Engine::default();
+    let component = match Component::from_file(&engine, "guest.wasm") {
+		Ok(lol) => lol,
+		Err(e) => {
+			println!("{}", e);
+			panic!("Fuck you");
+		}
+	};
+    let linker = Linker::new(&engine);
+    let mut store = Store::new(&engine, ());
+    let instance = match Plugin::instantiate(&mut store, &component, &linker) {
+		Ok(x) => x,
+		Err(_y) => panic!("Fcuk")
+	};
+
+	let x = instance.interface0.call_baz(&mut store);
+    Ok(())
+}
+
